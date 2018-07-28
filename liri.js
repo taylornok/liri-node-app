8276f01fd298cac2ad52c5a01dcb4066dee42a5d
taylornok <taylorowenskees@outlook.com>
2018-07-27 17:23:28 -0700

    twitter and spotify done

--- liri.js
+++ liri.js
@@ -12,25 +12,37 @@ let twitter = new Twitter(keys.twitter);
 let command = process.argv[2];
 let searchTopic = process.argv[3];
 
-//Develop functions based on userInput
+//Develop functions to display data based on userInput
+//For spotify 
+
+const displaySongInfo = () => {
+  let artist = data.tracks.items[0].artists[0].name;
+  let song = data.tracks.items[0].name;
+  let preview = data.tracks.items[0].artists[0].preview_url;
+
+  console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.' + 'Here\'s a preview of the top search result! \n' + preview)
+
+}
 
 //my-tweets function
 if (command === 'my-tweets') {
-    const params = {screen_name: 'purpleheffalum1'};
+  const params = {
+    screen_name: 'purpleheffalum1'
+  };
 
-    twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+  twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
       for (let i = 0; i < tweets.length; i++) {
         console.log(tweets[i].text);
       }
-    }else{
+    } else {
       console.log('There\'s been an error')
     }
   });
 
 }
 //spotify-this-song
-else if(command === 'spotify-this-song'){
+else if (command === 'spotify-this-song') {
   let nodeArgs = process.argv
   let searchTopic = '';
   for (let i = 3; i < nodeArgs.length; i++) {
@@ -39,24 +51,31 @@ else if(command === 'spotify-this-song'){
     // console.log(searchTopic) 
   }
   searchTopic.trim();
-  spotify.search({ type: 'track', query: searchTopic, limit: 5}, function(err, data) {
+  spotify.search({
+    type: 'track',
+    query: searchTopic,
+    limit: 5
+  }, function (err, data) {
     if (!err) {
-      // let spotData = data;
+      let artist = data.tracks.items[0].artists[0].name;
+      let song = data.tracks.items[0].name;
+      let preview = data.tracks.items[0].preview_url;
+      
+    
+      console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'Here\'s a preview of the top search result! \n' + preview)
 
-      //   console.log(spotData.tracks.items[i].name);
-      //   console.log(spotData.tracks.items[i].preview_url);
-
-      console.log('\nYou entered: ' + data.tracks.items[0].name + '\nThat sounds like a cool song!'); 
-      console.log('\nThe spotify search found a song by: ' + data.tracks.items[0].artists[3]);
-      console.log('Here\'s a preview of the top search result! \n' + data.tracks.items[0].preview_url);
-    }else{
+    } else {
       return console.log('Error occurred: ' + err);
     }
   });
 }
+
+
+
+
 // }else if(command === 'movie-this'){
 //   run OMDB api;
 // }
 // else if(command = 'do-what-it-says'){
-  
+
 // }
