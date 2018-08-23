304c13bf496b084349ac5a10835083899e29cd69
taylornok <taylorowenskees@outlook.com>
2018-08-22 20:09:05 -0700

    finished!

--- liri.js
+++ liri.js
@@ -13,8 +13,9 @@ let spotify = new Spotify(keys.spotify);
 let twitter = new Twitter(keys.twitter);
 
 //Develop variables to capture data based on userInput
-let command = process.argv[2];
-let searchTopic = process.argv[3];
+// let command = ;
+// let searchTopic = process.argv[3];
+handleRequest(process.argv[2], process.argv[3]);
 
 //Give users the directions for interacting with the command line
 const greetUser = () => {
@@ -33,140 +34,286 @@ const greetUser = () => {
 
 
 //Only give the directions if the user has not entered any arguements - alt to null message
-if (!command) {
-  greetUser();
-}
+// if (!command) {
+//   greetUser();
+// }
 
-//my-tweets access
-if (command === 'my-tweets') {
-  const params = {
-    screen_name: 'purpleheffalum1'
-  };
+// //my-tweets access
+// if (command === 'my-tweets') {
+//   const params = {
+//     screen_name: 'purpleheffalum1'
+//   };
 
-  twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      for (let i = 0; i < tweets.length; i++) {
-        console.log('\n' + i + '-' + tweets[i].text + '\n^Created at: ' + tweets[i].created_at + '\n');
-      }
-    } else {
-      console.log('There\'s been an error')
-    }
-  });
+//   twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
+//     if (!error) {
+//       for (let i = 0; i < tweets.length; i++) {
+//         console.log('\n' + i + '-' + tweets[i].text + '\n^Created at: ' + tweets[i].created_at + '\n');
+//       }
+//     } else {
+//       console.log('There\'s been an error')
+//     }
+//   });
 
-}
-
-
-//spotify-this-song access
-else if (command === 'spotify-this-song') {
-  let nodeArgs = process.argv
-  let searchTopic = '';
+// }
 
 
-  for (let i = 3; i < nodeArgs.length; i++) {
+// //spotify-this-song access
+// else if (command === 'spotify-this-song') {
+//   let nodeArgs = process.argv
+//   let searchTopic = '';
 
-    // Build a string with the search letters, joined by ' '
-    searchTopic += ' ' + nodeArgs[i];
 
-  }
-  searchTopic.trim();
-  if (searchTopic === '') {
-    searchTopic = 'The Sign Ace of Base';
-  }
+//   for (let i = 3; i < nodeArgs.length; i++) {
 
-  spotify.search({
-      type: 'track',
-      query: searchTopic,
-      limit: 5
-    }, function (err, data) {
-      if (!err) {
-        if (data.tracks.items.length > 1) {
-          // console.log(data)
-          let artist = data.tracks.items[0].artists[0].name
-          // console.log(artist);
+//     // Build a string with the search letters, joined by ' '
+//     searchTopic += ' ' + nodeArgs[i];
 
-          let song = data.tracks.items[0].name;
-          // console.log(song)
-          let preview = data.tracks.items[0].preview_url;
-          // console.log(preview)
-          let album = data.tracks.items[0].album.name;
-          // console.log(album)
-          console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'It comes from the album: ' + album + '\nHere\'s a preview of the top search result! \n' + preview)
+//   }
+//   searchTopic.trim();
+//   if (searchTopic === '') {
+//     searchTopic = 'The Sign Ace of Base';
+//   }
 
-        } else {
-          console.log('Sorry, I could\'nt find that one, try another song')
-        }
-      }
-    }
+//   spotify.search({
+//       type: 'track',
+//       query: searchTopic,
+//       limit: 5
+//     }, function (err, data) {
+//       if (!err) {
+//         if (data.tracks.items.length > 1) {
+//           // console.log(data)
+//           let artist = data.tracks.items[0].artists[0].name
+//           // console.log(artist);
 
-  )
-} else if (command === 'movie-this') {
-  let nodeArgs = process.argv
-  let searchTopic = '';
+//           let song = data.tracks.items[0].name;
+//           // console.log(song)
+//           let preview = data.tracks.items[0].preview_url;
+//           // console.log(preview)
+//           let album = data.tracks.items[0].album.name;
+//           // console.log(album)
+//           console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'It comes from the album: ' + album + '\nHere\'s a preview of the top search result! \n' + preview)
 
-  for (let i = 3; i < nodeArgs.length; i++) {
-    // Build a string with the search letters, joined by ' ', will be fed into search parameters
-    searchTopic += '+' + nodeArgs[i];
-  }
-  searchTopic.trim();
+//         } else {
+//           console.log('Sorry, I could\'nt find that one, try another song')
+//         }
+//       }
+//     }
 
-  request('http://www.omdbapi.com/?t=' + searchTopic + '&apikey=trilogy', function (err, response, body) {
-    if (!err) {
+//   )
+// } else if (command === 'movie-this') {
+//   let nodeArgs = process.argv
+//   let searchTopic = '';
 
-      let jsonData = JSON.parse(body);
+//   for (let i = 3; i < nodeArgs.length; i++) {
+//     // Build a string with the search letters, joined by ' ', will be fed into search parameters
+//     searchTopic += '+' + nodeArgs[i];
+//   }
+//   searchTopic.trim();
 
-      console.log(
-        '\nTitle: ' + jsonData.Title,
-        '\nRelease Year: ' + jsonData.Year,
-        '\nIMDB Rating: ' + jsonData.Ratings[0].Value,
-        '\nRotten Tomatoes Rating: ' + jsonData.Ratings[1].Value,
-        '\nCountry: ' + jsonData.Country,
-        '\nLanguage: ' + jsonData.Language,
-        '\nPlot: ' + jsonData.Plot,
-        '\nActors: ' + jsonData.Actors
-      )
+//   request('http://www.omdbapi.com/?t=' + searchTopic + '&apikey=trilogy', function (err, response, body) {
+//     if (!err) {
 
-    }
-    if (err) {
-      console.log(err);
-    }
-  })
+//       let jsonData = JSON.parse(body);
 
-} else if (command === 'do-what-it-says') {
-  FS.readFile('random.txt', 'utf8', function (error, data) {
-    if (error) {
-      return console.log(error);
+//       console.log(
+//         '\nTitle: ' + jsonData.Title,
+//         '\nRelease Year: ' + jsonData.Year,
+//         '\nIMDB Rating: ' + jsonData.Ratings[0].Value,
+//         '\nRotten Tomatoes Rating: ' + jsonData.Ratings[1].Value,
+//         '\nCountry: ' + jsonData.Country,
+//         '\nLanguage: ' + jsonData.Language,
+//         '\nPlot: ' + jsonData.Plot,
+//         '\nActors: ' + jsonData.Actors
+//       )
 
-    }else{
-  
-      let dataArray = data.split(',');
-      let dataSong = (dataArray[1]);
+//     }
+//     if (err) {
+//       console.log(err);
+//     }
+//   })
+
+// } else if (command === 'do-what-it-says') {
+//   FS.readFile('random.txt', 'utf8', function (error, data) {
+//     if (error) {
+//       return console.log(error);
+
+//     }else{
       
-      spotify.search({
-        type: 'track',
-        query: dataSong,
-        limit: 5
-      }, function (err, repsonse) {
-        if (!err) {
-          console.log('inside spotify function ', dataSong)
-          if (response.tracks.items.length > 1) {
-            console.log(response)
-            let artist = response.tracks.items[0].artists[0].name
-            console.log(artist);
+//       console.log(data);
+//       let task = data.split(',')[0];
+//       let dataArray = data.split(',');
+//       let dataSong = (dataArray[1]);
+      
+//       spotify.search({
+//         type: 'track',
+//         query: dataSong,
+//         limit: 5
+//       }, function (err, response) {
+//         if (!err) {
+//           console.log('inside spotify function ', dataSong)
+//           if (response.tracks.items.length > 1) {
+//             console.log(response)
+//             let artist = response.tracks.items[0].artists[0].name
+//             console.log(artist);
   
-            let song = response.tracks.items[0].name;
-            console.log(song)
-            let preview = response.tracks.items[0].preview_url;
-            console.log(preview)
-            let album = response.tracks.items[0].album.name;
-            console.log(album)
+//             let song = response.tracks.items[0].name;
+//             console.log(song)
+//             let preview = response.tracks.items[0].preview_url;
+//             console.log(preview)
+//             let album = response.tracks.items[0].album.name;
+//             console.log(album)
+//             console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'It comes from the album: ' + album + '\nHere\'s a preview of the top search result! \n' + preview)
+  
+//           } else {
+//             console.log('Sorry, I could\'nt find that one, try another song')
+//           }
+
+//     }
+//   })
+//     }
+//   })
+// }
+
+function handleRequest (command, searchTopic) {
+  if (!command) {
+    greetUser();
+  }
+  
+  //my-tweets access
+  if (command === 'my-tweets') {
+    const params = {
+      screen_name: 'purpleheffalum1'
+    };
+  
+    twitter.get('statuses/user_timeline', params, function (error, tweets, response) {
+      if (!error) {
+        for (let i = 0; i < tweets.length; i++) {
+          console.log('\n' + i + '-' + tweets[i].text + '\n^Created at: ' + tweets[i].created_at + '\n');
+        }
+      } else {
+        console.log('There\'s been an error')
+      }
+    });
+  
+  }
+  
+  
+  //spotify-this-song access
+  else if (command === 'spotify-this-song') {
+    // let nodeArgs = process.argv
+    // let searchTopic = '';
+  
+  
+    // for (let i = 3; i < nodeArgs.length; i++) {
+  
+    //   // Build a string with the search letters, joined by ' '
+    //   searchTopic += ' ' + nodeArgs[i];
+  
+    // }
+    // searchTopic.trim();
+    if (searchTopic === '') {
+      searchTopic = 'The Sign Ace of Base';
+    }
+  
+    spotify.search({
+        type: 'track',
+        query: searchTopic,
+        limit: 5
+      }, function (err, data) {
+        if (!err) {
+          if (data.tracks.items.length > 1) {
+            // console.log(data)
+            let artist = data.tracks.items[0].artists[0].name
+            // console.log(artist);
+  
+            let song = data.tracks.items[0].name;
+            // console.log(song)
+            let preview = data.tracks.items[0].preview_url;
+            // console.log(preview)
+            let album = data.tracks.items[0].album.name;
+            // console.log(album)
             console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'It comes from the album: ' + album + '\nHere\'s a preview of the top search result! \n' + preview)
   
           } else {
             console.log('Sorry, I could\'nt find that one, try another song')
           }
-
+        }
+      }
+  
+    )
+  } else if (command === 'movie-this') {
+    let nodeArgs = process.argv
+    let searchTopic = '';
+  
+    for (let i = 3; i < nodeArgs.length; i++) {
+      // Build a string with the search letters, joined by ' ', will be fed into search parameters
+      searchTopic += '+' + nodeArgs[i];
     }
-  })
-    }
-  })
+    searchTopic.trim();
+  
+    request('http://www.omdbapi.com/?t=' + searchTopic + '&apikey=trilogy', function (err, response, body) {
+      if (!err) {
+  
+        let jsonData = JSON.parse(body);
+  
+        console.log(
+          '\nTitle: ' + jsonData.Title,
+          '\nRelease Year: ' + jsonData.Year,
+          '\nIMDB Rating: ' + jsonData.Ratings[0].Value,
+          '\nRotten Tomatoes Rating: ' + jsonData.Ratings[1].Value,
+          '\nCountry: ' + jsonData.Country,
+          '\nLanguage: ' + jsonData.Language,
+          '\nPlot: ' + jsonData.Plot,
+          '\nActors: ' + jsonData.Actors
+        )
+  
+      }
+      if (err) {
+        console.log(err);
+      }
+    })
+  
+  } else if (command === 'do-what-it-says') {
+    FS.readFile('random.txt', 'utf8', function (error, data) {
+      if (error) {
+        return console.log(error);
+  
+      }else{
+        // console.log(data);
+        let task = data.split(',')[0];
+        let query = data.split(', ')[1];
+        let dataArray = data.split(',');
+        let dataSong = (dataArray[1]);
+        console.log(task , query);
+        handleRequest(task, query)
+        // spotify.search({
+        //   type: 'track',
+        //   query: dataSong,
+        //   limit: 5
+        // }, function (err, response) {
+        //   if (!err) {
+        //     console.log('inside spotify function ', dataSong)
+        //     if (response.tracks.items.length > 1) {
+        //       console.log(response)
+        //       let artist = response.tracks.items[0].artists[0].name
+        //       console.log(artist);
+    
+        //       let song = response.tracks.items[0].name;
+        //       console.log(song)
+        //       let preview = response.tracks.items[0].preview_url;
+        //       console.log(preview)
+        //       let album = response.tracks.items[0].album.name;
+        //       console.log(album)
+        //       console.log('\nYou entered: ' + song + '.' + '\nThat sounds like a cool song!' + '\nThe spotify search found a song by: ' + artist + '.\n' + 'It comes from the album: ' + album + '\nHere\'s a preview of the top search result! \n' + preview)
+    
+            // } else {
+            //   console.log('Sorry, I could\'nt find that one, try another song')
+            // }
+  
+    //   }
+    // })
+      }
+    })
+  }
 }
+
